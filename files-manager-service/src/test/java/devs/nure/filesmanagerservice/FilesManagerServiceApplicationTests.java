a1b64731582aefd33ad605031d574227521d88b8
Al_Sah <aplexidr@gmail.com>
2021-01-13 19:21:32 +0200

    Init files-manager-service

---
+++ files-manager-service/src/test/java/devs/nure/filesmanagerservice/FilesManagerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package devs.nure.filesmanagerservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FilesManagerServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
