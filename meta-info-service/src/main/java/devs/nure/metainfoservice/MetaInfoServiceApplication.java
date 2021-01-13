c2663c1f5f42fd1097edc82f431736688912da76
Al_Sah <aplexidr@gmail.com>
2021-01-13 16:56:00 +0200

    Init meta-info-service

---
+++ meta-info-service/src/main/java/devs/nure/metainfoservice/MetaInfoServiceApplication.java
@@ -0,0 +1,13 @@
+package devs.nure.metainfoservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MetaInfoServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MetaInfoServiceApplication.class, args);
+    }
+
+}
---
+++ meta-info-service/src/test/java/devs/nure/metainfoservice/MetaInfoServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package devs.nure.metainfoservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MetaInfoServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
