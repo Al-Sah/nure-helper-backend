337e7b02fac9878ec7bd1cc161a3db0d09e949c3
zeliboben <daniil.ko028@gmail.com>
2021-02-01 14:44:04 +0200

    Added circuit breaker for uploadFile controller

--- files-manager-service/src/main/java/devs/nure/filesmanagerservice/controllers/FilesController.java
+++ files-manager-service/src/main/java/devs/nure/filesmanagerservice/controllers/FilesController.java
@@ -1,13 +1,21 @@
 package devs.nure.filesmanagerservice.controllers;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 import devs.nure.filesmanagerservice.services.FilesService;
 import devs.nure.formslibrary.*;
+import org.springframework.core.io.AbstractResource;
 import org.springframework.core.io.Resource;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
 
 @RestController
 @RequestMapping("/api/v1/files")
@@ -26,6 +34,7 @@ public class FilesController {
     }
 
     @GetMapping("/{fileID}")
+    @HystrixCommand(fallbackMethod = "fallbackMethodUploadFile")
     public ResponseEntity<Resource> uploadFile(@PathVariable String fileID){
         Resource resource = filesService.downloadFile(fileID);
         FileInfo info = filesService.getFileInfo(fileID);
@@ -36,9 +45,30 @@ public class FilesController {
                 .body(resource);
     }
 
+    public ResponseEntity<Resource> fallbackMethodUploadFile(@PathVariable String fileID){
+        return new ResponseEntity<Resource>(new AbstractResource() {
+            @Override
+            public String getDescription() {
+                return null;
+            }
+
+            @Override
+            public InputStream getInputStream() throws IOException {
+                return new ByteArrayInputStream(
+                        ("this is fallback\n" + HttpStatus.REQUEST_TIMEOUT.toString())
+                                .getBytes(StandardCharsets.UTF_8));
+            }
+        }, HttpStatus.REQUEST_TIMEOUT);
+    }
+
     @DeleteMapping("/remove")
     public void removeFile(@RequestParam("fileId") String fileId) {
         filesService.removeFile(fileId);
     }
 
+
+    @ExceptionHandler // TODO ControllerAdvice
+    public ResponseEntity<ErrorMessage> handleException(RuntimeException exception) {
+        return new ResponseEntity<>(new ErrorMessage(exception.getMessage()), HttpStatus.BAD_REQUEST);
+    }
 }
